6cf30800460948e8d3a27d6d7470163c62889c78
Sachin <drsachin6705@gmail.com>
2025-09-11 22:16:22 +0530

    Donation file update

--- src/main/java/com/foodwaste/model/Donation.java
+++ src/main/java/com/foodwaste/model/Donation.java
@@ -1,5 +1,82 @@
 package com.foodwaste.model;
 
 public class Donation {
+	public int id;
+	public int donorid;
+	public String foodtype;
+	public String quantity;
+	public String pickup;
+	public String status;
+	
+	public Donation() {}
+	
+	public Donation(int id,int donorid,String foodtype,String quantity,String pickup,String status) {
+		this.id=id;
+		this.donorid=donorid;
+		this.foodtype=foodtype;
+		this.quantity=quantity;
+		this.pickup=pickup;
+		this.status=status;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	
+	
+	public int getDonorid()
+	{
+		return donorid;
+	}
+	public void setDonorid(int donorid)
+	{
+		this.donorid=donorid;
+	}
+	
+	
+	public String getFoodtype()
+	{
+		return foodtype;
+	}
+	public void setFoodtype(String foodtype)
+	{
+		this.foodtype=foodtype;
+	}
+	
+	
+	public String getQuantity()
+	{
+		return quantity;
+	}
+	public void setQUantity(String quantity)
+	{
+		this.quantity=quantity;
+	}
+	
+	
+	public String getPickup()
+	{
+		return pickup;
+	}
+	public void setPickup(String pickup)
+	{
+		this.pickup=pickup;
+	}
+	
+	
+	public String getStatus()
+	{
+		return status;
+	}
+	public void setStatus(String status)
+	{
+		this.status=status;
+	}
+	
 
 }
