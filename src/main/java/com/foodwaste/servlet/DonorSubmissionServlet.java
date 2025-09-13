387ac2f0569c3664dce392f4fa4bbe8f738f66a8
Sachin <drsachin6705@gmail.com>
2025-09-13 21:39:37 +0530

    New Servlet update

--- src/main/java/com/foodwaste/servlet/DonorSubmissionServlet.java
+++ src/main/java/com/foodwaste/servlet/DonorSubmissionServlet.java
@@ -1,5 +1,26 @@
 package com.foodwaste.servlet;
 
-public class DonorSubmissionServlet {
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import org.json.JSONObject;
+import com.foodwaste.model.*;
+import java.util.*;
 
+
+public class DonorSubmissionServlet extends HttpServlet{
+	private static final long serialVersionUID=1L;
+	
+	public static List<Donation>donations=new ArrayList<>();
+	
+	
+	@Override
+	 protected void doPost(HttpServletRequest request,HttpServletResponse response) 
+	throws ServletException,IOException{
+		
+		response.setContentType("applicarion/json");
+		PrintWriter out =response.getWriter();
+		
+	}
 }
+
