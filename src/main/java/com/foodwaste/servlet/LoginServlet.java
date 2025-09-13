6848496bc9379cc2b031804a6e5e1b26d6a91ffd
Sachin <drsachin6705@gmail.com>
2025-09-13 19:59:05 +0530

    LoginServlet update to store temperory data

--- src/main/java/com/foodwaste/servlet/LoginServlet.java
+++ src/main/java/com/foodwaste/servlet/LoginServlet.java
@@ -5,12 +5,33 @@ import javax.servlet.http.*;
 
 import java.io.*;
 import org.json.JSONObject;
+import com.foodwaste.model.*;
+import java.util.*;
 
 public class LoginServlet extends HttpServlet {
-    /**
-	 * 
-	 */
+    
 	private static final long serialVersionUID = 1L;
+	
+	private static List<User> users=new ArrayList<>();
+	public static void addUser(User user) {
+		users.add(user);
+	}
+	
+	
+	
+	private User authenticate(String email,String password) {
+		
+		for(User user:users) {
+			
+			if (user.getEmail().equalsIgnoreCase(email)&&user.getPassword().equals(password)) {
+				
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	@Override
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -21,7 +42,9 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         JSONObject json = new JSONObject();
-        if (email.equals("sachin") && password.equals("pass123")) {
+        
+        User user = authenticate(email,password);
+        if (user !=null) {
             json.put("status", "success");
             json.put("message", "Login successful!");
         } else {
